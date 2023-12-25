3a1e2e224aaa298b4aad1c1fe86776b6c3489ff0
Threshant <threshant@gmail.com>
2023-12-25 16:58:01 +0530

    Updated vars

--- lexer.py
+++ lexer.py
@@ -1,46 +1,49 @@
 import constant
 from token import Token
 class Lexer:
-    def __init__(self) -> None:
+    def __init__(self):
         self.tokens = []
         pass
     
     def appendToken(self,word, type):
-            if(len(word)>0):
-                word = word.rstrip()
-                word = word.lstrip()
-                self.tokens.append(Token(type,word))
+        if(len(word)>0):
+            word = word.rstrip()
+            word = word.lstrip()
+            self.tokens.append(Token(type,word))
+        pass
+            
+    def discriminateDigitandID(self,word):
+        if(word.isdigit()):
+            self.appendToken(word,"NUMBER")
+        else:
+            self.appendToken(word,"ID")
+        pass
+
+
     def getTokens(self):
         return self.tokens
     def lex(self,f):
         content = f.read()
         word = ""
-        cnt = 0
+        quotes_count = 0
         for i in content:
             if(i not in constant.operators and i !='\n'):
                 word+=i
             if(i=='"'):
-                cnt+=1
+                quotes_count+=1
             if(i=="\n"):
-                if(word.strip().isdigit()):
-                    self.appendToken(word,"NUMBER")
-                else:
-                    self.appendToken(word,"ID")
+                self.discriminateDigitandID(word)
                 word = ""
-                cnt = 0
+                quotes_count = 0
                 continue
-            if(cnt==2):
+            if(quotes_count==2):
                 self.appendToken(word,"STRING")
-                cnt = 0
+                quotes_count = 0
                 word = ""
                 continue
-            if(cnt == 0):
+            if(quotes_count == 0):
                 if(i in constant.operators):
-                    if(word.isdigit()):
-                        self.appendToken(word,"NUMBER")
-                    else:
-                        self.appendToken(word,"ID")
-                    
+                    self.discriminateDigitandID(word)
                     self.appendToken(i,constant.operators.get(i))
                     word = ""
                 if(word.strip() in constant.keywords):
